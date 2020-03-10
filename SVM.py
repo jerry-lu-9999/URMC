dbe4f57951e08a2c02d6e7d00ade45e4ed1729b6
yanyujianshen <ljh274163788@gmail.com>
2020-03-10 16:53:29 -0400

    debug py file

--- SVM.py
+++ SVM.py
@@ -14,25 +14,29 @@ from sklearn import model_selection, naive_bayes, svm
 
 np.random.seed(500)
 
-corpus = pd.read_csv(r"C:\Users\jlu39\Desktop\prevDev.csv", usecols=[2,9,13], encoding='latin-1')
+corpus = pd.read_excel("/Users/jiahaolu/Desktop/2017 Master Notes Data.12.9.19.xlsx", 
+                        header = 0,
+                        usecols = "A,C,S,W")
+print(corpus.head(5))
 
 #remove any possible blank rows and convert into lower case
-corpus['text'].dropna(inplace=True)
-corpus['text'] = [entry.lower() for entry in corpus['text']]
+corpus['note'].dropna(inplace=True)
+corpus['note'] = [entry.lower() for entry in corpus['note']]
 #each entry broken into set of words
-corpus['text'] = [word_tokenize(entry) for entry in corpus['text']]
+corpus['note'] = [word_tokenize(entry) for entry in corpus['note']]
 
-tag_map = defaultdict[lambda : wn.NOUN]
+print(corpus.head(5))
+tag_map = defaultdict(lambda : wn.NOUN)
 tag_map['J'] = wn.ADJ
 tag_map['V'] = wn.VERB
 tag_map['R'] = wn.ADV
 
-for index,entry in enumerate(corpus['text']):
+for index,entry in enumerate(corpus['note']):
     Final_words = []
     word_lemmatized = WordNetLemmatizer()
     for word, tag in pos_tag(entry):
         if word not in stopwords.words('english') and word.isalpha():
             word_Final = word_lemmatized.lemmatize(word, tag_map[tag[0]])
             Final_words.append(word_Final)
-    corpus.loc[index, 'text_final'] = str(Final_words)
-
+    corpus.loc[index, 'note_final'] = str(Final_words)
+print(corpus.head(5))
