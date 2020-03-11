08d58581d844842d9e1c6f5a64c514bb02a425f1
yanyujianshen <ljh274163788@gmail.com>
2020-03-11 16:08:42 -0400

    finish up the sample code

--- SVM.py
+++ SVM.py
@@ -26,6 +26,8 @@ corpus['note'] = [entry.lower() for entry in corpus['note']]
 corpus['note'] = [word_tokenize(entry) for entry in corpus['note']]
 
 print(corpus.head(5))
+
+#pos tag for words, default words is a NOUN
 tag_map = defaultdict(lambda : wn.NOUN)
 tag_map['J'] = wn.ADJ
 tag_map['V'] = wn.VERB
@@ -39,4 +41,29 @@ for index,entry in enumerate(corpus['note']):
             word_Final = word_lemmatized.lemmatize(word, tag_map[tag[0]])
             Final_words.append(word_Final)
     corpus.loc[index, 'note_final'] = str(Final_words)
-print(corpus.head(5))
+
+Train_X, Test_X, Train_Y, Test_Y = model_selection.train_test_split(corpus['note_final'], 
+                                                                    corpus['Fever?'], 
+                                                                    test_size = 0.3)
+
+Encoder = LabelEncoder()
+#fit label encoder and return encoded labels
+Train_Y = Encoder.fit_transform(Train_Y)
+Test_Y = Encoder.fit_transform(Test_Y)
+
+#Using TF-IDF    Term frequency-Inverse Document Frequency
+Tfidf_vect = TfidfVectorizer(max_features=5000)
+Tfidf_vect.fit(corpus['note_final'])
+
+Train_X_Tfidf = Tfidf_vect.transform(Train_X)
+Test_X_Tfidf = Tfidf_vect.transform(Test_X)
+
+print(Tfidf_vect.vocabulary_)
+
+#SVM
+SVM = svm.SVC(C=1.0, kernel='linear', degree=3, gamma='auto')
+SVM.fit(Train_X_Tfidf, Train_Y)
+
+predictions_SVM = SVM.predict(Test_X_Tfidf)
+
+print("SVM Accuracy Score ->" , accuracy_score(predictions_SVM, Test_Y) * 100)
