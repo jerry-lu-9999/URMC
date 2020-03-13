b6880a3da91c112a75362c69889eb64bf02d5a9b
yanyujianshen <ljh274163788@gmail.com>
2020-03-13 15:28:28 -0400

    trying to figure out how to loop through the entire table

--- SVM.py
+++ SVM.py
@@ -1,23 +1,32 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
-from nltk.tokenize import word_tokenize
+from timer import Timer
 from nltk import pos_tag
 from nltk.corpus import stopwords
-from nltk.stem import WordNetLemmatizer
-from _collections import defaultdict
 from nltk.corpus import wordnet as wn
-from sklearn.feature_extraction.text import  TfidfVectorizer
+from nltk.stem import WordNetLemmatizer
+from nltk.tokenize import word_tokenize
+from sklearn import model_selection, naive_bayes, svm
+from sklearn.feature_extraction.text import HashingVectorizer, TfidfVectorizer
 from sklearn.metrics import accuracy_score
 from sklearn.preprocessing import LabelEncoder
-from sklearn import model_selection, naive_bayes, svm
+
+from _collections import defaultdict
 
 np.random.seed(500)
 
-corpus = pd.read_excel("/Users/jiahaolu/Desktop/2017 Master Notes Data.12.9.19.xlsx", 
+
+corpus = pd.read_csv(r"/Users/jiahaolu/Desktop/2017 Master Notes Data.12.9.19.csv", 
                         header = 0,
-                        usecols = "A,C,S,W")
+                        usecols = ['emrn', 'Fever?', 'pat_enc_csn_id', 'note'])
 print(corpus.head(5))
+print(type(corpus['Fever?']))
+for index, entry in enumerate(corpus['emrn']):
+    if corpus['emrn'][index] == corpus['emrn']['index']:
+        print("yess it matches")
+    else:
+        print(" no it doesn't")
 
 #remove any possible blank rows and convert into lower case
 corpus['note'].dropna(inplace=True)
@@ -45,25 +54,32 @@ for index,entry in enumerate(corpus['note']):
 Train_X, Test_X, Train_Y, Test_Y = model_selection.train_test_split(corpus['note_final'], 
                                                                     corpus['Fever?'], 
                                                                     test_size = 0.3)
+print(Train_Y)
+# Encoder = LabelEncoder()
+# #fit label encoder and return encoded labels
+# #Y in our case is fever or not(T/F), X in our case is the doctor's notes
+# Train_Y = Encoder.fit_transform(Train_Y)
+# Test_Y = Encoder.fit_transform(Test_Y)
 
-Encoder = LabelEncoder()
-#fit label encoder and return encoded labels
-Train_Y = Encoder.fit_transform(Train_Y)
-Test_Y = Encoder.fit_transform(Test_Y)
+# #Using TF-IDF    Term frequency-Inverse Document Frequency    Turning text data to vectors
+# #Tfidf_vect = TfidfVectorizer(max_features=5000)
+# #Tfidf_vect.fit(corpus['note_final'])
+# Hash_vect = HashingVectorizer()
+# Hash_vect.fit(corpus['note_final'])
 
-#Using TF-IDF    Term frequency-Inverse Document Frequency
-Tfidf_vect = TfidfVectorizer(max_features=5000)
-Tfidf_vect.fit(corpus['note_final'])
+# # Train_X_Tfidf = Tfidf_vect.transform(Train_X)
+# # Test_X_Tfidf = Tfidf_vect.transform(Test_X)
 
-Train_X_Tfidf = Tfidf_vect.transform(Train_X)
-Test_X_Tfidf = Tfidf_vect.transform(Test_X)
+# Train_X_Tfidf = Hash_vect.transform(Train_X)
+# Test_X_Tfidf = Hash_vect.transform(Test_X)
+# print(Test_X.shape)
+# # print(Tfidf_vect.vocabulary_)
+# # print(Hash_vect.vocabulary_)
 
-print(Tfidf_vect.vocabulary_)
+# #SVM
+# SVM = svm.SVC(C=1.0, kernel='linear', degree=3, gamma='auto')
+# SVM.fit(Train_X_Tfidf, Train_Y)
 
-#SVM
-SVM = svm.SVC(C=1.0, kernel='linear', degree=3, gamma='auto')
-SVM.fit(Train_X_Tfidf, Train_Y)
+# predictions_SVM = SVM.predict(Test_X_Tfidf)
 
-predictions_SVM = SVM.predict(Test_X_Tfidf)
-
-print("SVM Accuracy Score ->" , accuracy_score(predictions_SVM, Test_Y) * 100)
+# print("SVM Accuracy Score ->" , accuracy_score(predictions_SVM, Test_Y) * 100)
